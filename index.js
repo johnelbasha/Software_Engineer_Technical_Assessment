65f75e65fe26edffa20d5d1e684335befcd30e9a
John El Basha <johnelbasha@gmail.com>
2021-02-04 06:28:41 +0000

    bunch of dummy tests have been run and pass just to establish testing work-flow

---
+++ index.js
@@ -0,0 +1,9 @@
+function sayHello() {
+    return 'hello';
+};
+function addNumbers(x, y) {
+    return x + y;
+};
+
+module.exports.sayHello = sayHello;
+module.exports.addNumbers = addNumbers;
--- test/IndexTest.js
+++ test/IndexTest.js
@@ -1,7 +1,19 @@
 const assert = require('chai').assert;
+const app = require('../index');
+// Results
+sayHelloResult = app.sayHello();
+addNumbersResult = app.addNumbers(5,5);
 
 describe('App', function(){
-    it('app should return hello', function(){
-        assert.equal(App(), 'hello') ;
+    it('sayHello should return hello', function(){
+        assert.equal(sayHelloResult, 'hello') ;
+    });
+
+    it('sayHello should return a string', function(){
+        assert.typeOf(sayHelloResult, 'string');
+    });
+
+    it('addNumbers should above 5', function(){
+        assert.isAbove(addNumbersResult, 5);
     });
 });
