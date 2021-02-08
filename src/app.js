4cb837a4fe93f4f1b4e00239661e64980ed90e38
John El Basha <johnelbasha@gmail.com>
2021-02-08 00:38:57 +0000

    further refactored app.js

--- src/app.js
+++ src/app.js
@@ -1,27 +1,5 @@
-const validDataArg = [
-    {
-        monthNumber: 0, // current month
-        account: {
-            balance: { amount: 0 },
-        },
-    },
-    {
-        monthNumber: 1, // last month
-        account: {
-            balance: { amount: 100 },
-        },
-    },
-    {
-        monthNumber: 2, // two months ago
-        account: {
-            balance: { amount: 200 },
-        },
-    }
-]
-
-function accountTypeChecker(data) {
-    if (
-        Array.isArray(data) &&
+function isArgumentValid(data){
+    if(Array.isArray(data) &&
         data.length >= 3 &&
         data.every(child => child.constructor == Object &&
                             Object.keys(child).length === 2 &&
@@ -31,27 +9,43 @@ function accountTypeChecker(data) {
                             child['account']['balance'].constructor == Object &&
                             Object.keys(child['account']['balance']).length === 1 &&
                             typeof(child['account']['balance']['amount']) === 'number'
-        )
-    ) {
-        let amounts = [];
-        for (let child of data){
-            amounts.push(child['account']['balance']['amount']);
-        }
-        let deltas = [];
-        for (let i = 0; i < amounts.length - 1; i++) {
-            deltas.push(amounts[i+1] - amounts[i]);
-        }
-        if (deltas.every(diff => diff === deltas[0])){
-            return 'B';
-        } else {
-            return 'A';
-        } 
+                )
+    ){
+        return true;
     }
     else {
         return false;
     }
 }
 
-accountTypeChecker(validDataArg);
+function buildDifferencesArray(data) {
+    let amounts = [];
+    for (let child of data){
+        amounts.push(child['account']['balance']['amount']);
+    }
+    let deltas = [];
+    for (let i = 0; i < amounts.length - 1; i++) {
+        deltas.push(amounts[i+1] - amounts[i]);
+    }
+    return deltas;
+}
+
+function analyse(deltas){
+    if (deltas.every(diff => diff === deltas[0])){
+        return 'B';
+    } else {
+        return 'A';
+    } 
+}
+
+function accountTypeChecker(data) {
+    if (isArgumentValid(data)) {
+        deltas = buildDifferencesArray(data);
+        return analyse(deltas);
+    }
+    else {
+        return false;
+    }
+}
 
 module.exports.accountTypeChecker = accountTypeChecker;
